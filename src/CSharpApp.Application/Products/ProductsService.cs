022299be09ae917b815b68787e208cda1bcbf90c
Anastasios Moraitis <anas.moraitis@gmail.com>
2025-05-21 06:16:51 +0300

    Refactor code to use IHttpClientFactory pattern over HttpClient

--- src/CSharpApp.Application/Products/ProductsService.cs
+++ src/CSharpApp.Application/Products/ProductsService.cs
@@ -2,26 +2,32 @@ namespace CSharpApp.Application.Products;
 
 public class ProductsService : IProductsService
 {
-    private readonly HttpClient _httpClient;
     private readonly RestApiSettings _restApiSettings;
     private readonly ILogger<ProductsService> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
 
     public ProductsService(IOptions<RestApiSettings> restApiSettings, 
-        ILogger<ProductsService> logger)
+        ILogger<ProductsService> logger, IHttpClientFactory httpClientFactory)
     {
-        _httpClient = new HttpClient();
         _restApiSettings = restApiSettings.Value;
         _logger = logger;
+        _httpClientFactory = httpClientFactory;
     }
 
     public async Task<IReadOnlyCollection<Product>> GetProducts()
     {
-        _httpClient.BaseAddress = new Uri(_restApiSettings.BaseUrl!);
-        var response = await _httpClient.GetAsync(_restApiSettings.Products);
+        using var httpClient = _httpClientFactory.CreateClient(nameof(Product));
+        using var response = await httpClient.GetAsync(_restApiSettings.Products);
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
         var res = JsonSerializer.Deserialize<List<Product>>(content);
         
+        if (res is null)
+        {
+            _logger.LogError("Failed to deserialize response content");
+            throw new InvalidOperationException("Failed to deserialize response content");
+        }
+
         return res.AsReadOnly();
     }
 }
--- src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
@@ -1,9 +1,24 @@
+using CSharpApp.Core.Dtos;
+using Microsoft.Extensions.Options;
+
 namespace CSharpApp.Infrastructure.Configuration;
 
 public static class HttpConfiguration
 {
     public static IServiceCollection AddHttpConfiguration(this IServiceCollection services)
     {
+        services
+            .AddHttpClient(nameof(Product), (serviceProvider, httpClient) =>
+            {
+                var httpClientSettings = serviceProvider.GetRequiredService<IOptions<HttpClientSettings>>().Value;
+                var restApiSettings = serviceProvider.GetRequiredService<IOptions<RestApiSettings>>().Value;
+
+                httpClient.BaseAddress = new Uri(restApiSettings.BaseUrl!);
+                httpClient.Timeout = TimeSpan.FromSeconds(httpClientSettings.LifeTime);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
         return services;
     }
 }
